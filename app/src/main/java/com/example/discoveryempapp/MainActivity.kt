daca23407838df0774bfdd03a379b9f8bf06eeba
amitgangadeya <AmitG@discovery.co.za>
2023-03-29 14:30:56 +0200

    update

---
+++ app/src/main/java/com/example/discoveryempapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.discoveryempapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.os.Build
+
+
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+private lateinit var auth: FirebaseAuth
+class MainActivity : AppCompatActivity() {
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var logintext:EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Initialize Firebase Auth
+       auth = Firebase.auth
+
+        emailEditText = findViewById(R.id.username)
+        passwordEditText = findViewById(R.id.password)
+        loginButton = findViewById(R.id.log)
+        // logintext= findViewById(R.id.welcome)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            loginUser(email, password)
+        }
+    }
+    private fun loginUser(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    Toast.makeText(baseContext, "Authenticated!!!.",
+                        Toast.LENGTH_SHORT).show()
+/*
+                    val intent = Intent(
+                        this,
+                        com.example.discoveryempapp.Menu::class.java
+                    )
+ */
+                    startActivity(intent)
+
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+}
